b952891cf8c14a5f23b8d8db00e594515476cac4
Suhail Najeeb <suhailnajeeb19@gmail.com>
2019-10-13 21:15:32 +0600

    plot tsv file

--- testData.py
+++ testData.py
@@ -1,4 +1,31 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
 filepath = 'test.tsv'
-data = pd.read_csv(filepath, sep = '\t')
-data.shape
+
+columns = ['sample', 'time', 'x', 'y', 'z', 'instance']
+
+data = pd.read_csv(filepath, sep = '\t', names = columns)
+
+data.head()
+
+def plot_activity(activity, data):
+    fig, (ax0, ax1, ax2) = plt.subplots(nrows=3, figsize=(15, 7), sharex=True)
+    plot_axis(ax0, data['time'], data['x'], 'X-Axis')
+    plot_axis(ax1, data['time'], data['y'], 'Y-Axis')
+    plot_axis(ax2, data['time'], data['z'], 'Z-Axis')
+    plt.subplots_adjust(hspace=0.2)
+    fig.suptitle(activity)
+    plt.subplots_adjust(top=0.90)
+    plt.show()
+
+def plot_axis(ax, x, y, title):
+    ax.plot(x, y, 'g')
+    ax.set_title(title)
+    ax.xaxis.set_visible(False)
+    ax.set_ylim([min(y) - np.std(y), max(y) + np.std(y)])
+    ax.set_xlim([min(x), max(x)])
+    ax.grid(True)
+
+plot_activity('jaywalking', data)
